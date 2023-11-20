f18fb3c8615c094cd4cbf82a188320c518919101
jmulq <james.mulqueeny@outlook.com>
2023-11-20 20:52:38 +0000

    script: network specific config generation
    have added a script that converts the original json files (from the subgraph) to network specific rust config files for the project

--- build.rs
+++ build.rs
@@ -1,22 +1,20 @@
 use anyhow::{Ok, Result};
 use regex::Regex;
-use substreams_ethereum::Abigen;
+use serde_json::Value;
 use std::fs;
+use substreams_ethereum::Abigen;
 
 fn main() -> Result<(), anyhow::Error> {
     let file_names = [
         "abi/erc20.abi.json",
+        "abi/pool.abi.json",
         "abi/registry.abi.json",
     ];
-    let file_output_names = [
-        "src/abi/erc20.rs",
-        "src/abi/registry.rs",
-    ];
+    let file_output_names = ["src/abi/erc20.rs", "src/abi/pool.rs", "src/abi/registry.rs"];
 
     let mut i = 0;
     for f in file_names {
-        let contents = fs::read_to_string(f)
-            .expect("Should have been able to read the file");
+        let contents = fs::read_to_string(f).expect("Should have been able to read the file");
 
         // sanitize fields and attributes starting with an underscore
         let regex = Regex::new(r#"("\w+"\s?:\s?")_(\w+")"#).unwrap();
@@ -26,8 +24,116 @@ fn main() -> Result<(), anyhow::Error> {
             .generate()?
             .write_to_file(file_output_names[i])?;
 
-        i = i+1;
+        i = i + 1;
     }
+
+    generate_network_config_from_json(
+        // TODO: This will eventually need to be dynamic when we support multiple networks.
+        "config/curve-finance-ethereum/configuration.json",
+        "./src/config/configuration.rs",
+    )
+    .expect("Should have been able to generate the network configuration file");
+
+    Ok(())
+}
+
+fn generate_network_config_from_json(path: &str, output_path: &str) -> Result<()> {
+    let json_contents = fs::read_to_string(path)
+        .expect("Should have been able to read the network configuration file");
+
+    let json: Value = serde_json::from_str(&json_contents)?;
+
+    let mut output = String::new();
+
+    // Generated file imports
+    output.push_str("use hex_literal::hex;\n\n");
+
+    if let Some(network) = json["network"].as_str() {
+        output.push_str(&format!("pub const NETWORK: &str = \"{}\";\n", network));
+    }
+    if let Some(default_network) = json["defaultNetwork"].as_str() {
+        output.push_str(&format!(
+            "pub const DEFAULT_NETWORK: &str = \"{}\";\n",
+            default_network
+        ));
+    }
+    if let Some(price_caching) = json["priceCaching"].as_u64() {
+        output.push_str(&format!(
+            "pub const PRICE_CACHING: u64 = {};\n",
+            price_caching
+        ));
+    }
+    if let Some(pool_info_contract) = json["poolInfoContract"].as_str() {
+        output.push_str(&format!(
+            "pub const POOL_INFO_CONTRACT: [u8; 20] = hex!(\"{}\");\n",
+            pool_info_contract.trim_start_matches("0x")
+        ));
+    }
+    if let Some(protocol_address) = json["protocolAddress"].as_str() {
+        output.push_str(&format!(
+            "pub const PROTOCOL_ADDRESS: [u8; 20] = hex!(\"{}\");\n",
+            protocol_address.trim_start_matches("0x")
+        ));
+    }
+    if let Some(crv_token_address) = json["crvTokenAddress"].as_str() {
+        output.push_str(&format!(
+            "pub const CRV_TOKEN_ADDRESS: [u8; 20] = hex!(\"{}\");\n",
+            crv_token_address.trim_start_matches("0x")
+        ));
+    }
+    if let Some(gauge_controller_address) = json["gaugeControllerContract"].as_str() {
+        output.push_str(&format!(
+            "pub const GAUGE_CONTROLLER_ADDRESS: [u8; 20] = hex!(\"{}\");\n",
+            gauge_controller_address.trim_start_matches("0x")
+        ));
+    }
+
+    // Generating constants for poolRegistry
+    if let Some(pool_registry) = json["poolRegistry"].as_array() {
+        output.push_str("pub const CONTRACTS: [[u8; 20]; 5] = [");
+        for pool in pool_registry {
+            let address = pool["address"]
+                .as_str()
+                .unwrap_or_default()
+                .trim_start_matches("0x");
+            output.push_str(&format!("hex!(\"{}\"), ", address));
+        }
+        output.push_str("];\n");
+    }
+
+    // Generating structs for missingOldPools
+    output.push_str("\n#[derive(Debug, Clone)]\npub struct PoolConfig {\n    pub name: &'static str,\n    pub address: [u8; 20],\n    pub lp_token: [u8; 20],\n    pub start_block: u64,\n}\n");
+
+    if let Some(missing_old_pools) = json["missingOldPools"].as_array() {
+        output.push_str("\npub static MISSING_OLD_POOLS_DATA: &[(&str, PoolConfig)] = &[\n");
+        for pool in missing_old_pools {
+            let name = pool["name"].as_str().unwrap_or_default();
+            let address = pool["address"]
+                .as_str()
+                .unwrap_or_default()
+                .trim_start_matches("0x");
+            let lp_token = pool["lpToken"]
+                .as_str()
+                .unwrap_or_default()
+                .trim_start_matches("0x");
+            let start_block = pool["startBlock"].as_str().unwrap_or_default();
+            output.push_str(&format!("(\"{}\", PoolConfig {{ name: \"{}\", address: hex!(\"{}\"), lp_token: hex!(\"{}\"), start_block: {} }}),\n", address, name, address, lp_token, start_block));
+        }
+        output.push_str("];\n");
+    }
+
+    // Generating an array for basePoolsLpToken
+    if let Some(base_pools_lp_token) = json["basePoolsLpToken"].as_array() {
+        output.push_str(format!("\npub static BASE_POOLS_LP_TOKEN: [[u8; 20]; {}] = [\n", base_pools_lp_token.len()).as_str());
+        for pool in base_pools_lp_token {
+            let name = pool["name"].as_str().unwrap_or_default();
+            let address = pool["address"].as_str().unwrap_or_default().trim_start_matches("0x");
+            output.push_str(&format!("hex!(\"{}\"), // {}\n", address, name));
+        }
+        output.push_str("];\n");
+    }
+
+    fs::write(output_path, output)?;
 
     Ok(())
 }
--- src/constants.rs
+++ src/constants.rs
@@ -1,26 +1,21 @@
 use hex_literal::hex;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
 use substreams::scalar::BigInt;
 
-// Chain Specific Contracts (Registries and Factories):
-// ________________________
-// TODO: Currently am hardcoding Ethereum Mainnet contracts.
-//       This should be changed to a more dynamic approach, similar to the subgraph.
-//       A config file could be used to specify the contracts for each chain.
-//       This constants file can then be generated via a script or at build or compile time.
-//       Need to look into the most appropriate solution.
-const POOLREGISTRY_CONTRACT: [u8; 20] = hex!("90e00ace148ca3b23ac1bc8c240c2a7dd9c2d7f5");
-const POOLREGISTRY_V2_CONTRACT: [u8; 20] = hex!("7d86446ddb609ed0f5f8684acf30380a356b2b4c");
-const CRYPTOSWAP_REGISTRY_CONTRACT: [u8; 20] = hex!("8F942C20D02bEfc377D41445793068908E2250D0");
-const METAPOOL_FACTORY_CONTRACT: [u8; 20] = hex!("B9fC157394Af804a3578134A6585C0dc9cc990d4");
-const CRYPTOPOOL_FACTORY: [u8; 20] = hex!("F18056Bbd320E96A48e3Fbf8bC061322531aac99");
+use crate::config::configuration::{PoolConfig, MISSING_OLD_POOLS_DATA};
 
-pub const CONTRACTS: [[u8; 20]; 5] = [
-    POOLREGISTRY_CONTRACT,
-    POOLREGISTRY_V2_CONTRACT,
-    CRYPTOSWAP_REGISTRY_CONTRACT,
-    METAPOOL_FACTORY_CONTRACT,
-    CRYPTOPOOL_FACTORY,
-];
+// Chain Specific Contracts:
+// ________________________
+// Most of the network specific addresses/config are stored in the configuration file.
+// See src/config/configuration.rs for more details.
+// Old pools needs to be stored as a hash map, so we lazily intiialise this at runtime.
+
+lazy_static! {
+    pub static ref MISSING_OLD_POOLS: HashMap<&'static str, PoolConfig> = {
+        MISSING_OLD_POOLS_DATA.iter().cloned().collect()
+    };
+}
 
 // Global Constants:
 // These will not be dynamic like the chain specific contracts above.
