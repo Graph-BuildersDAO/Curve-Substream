a171add73d02b7f70731d89ce1bc374030e5b107
jmulq <james.mulqueeny@outlook.com>
2024-02-02 16:32:57 +0000

    refactor: update event extraction functions
    - Improving `extract_specific_transfer_event` so you have more granular control over params.
    - Adding `extract_remove_liquidity_one_event` which is used for handling `ExchangeUnderlying` events

--- src/common/event_extraction.rs
+++ src/common/event_extraction.rs
@@ -4,26 +4,51 @@ use anyhow::anyhow;
 use substreams::errors::Error;
 use substreams_ethereum::{block_view, pb::eth::v2::TransactionTrace, Event};
 
-use crate::abi::common::erc20::events::Transfer;
+use crate::{
+    abi::{
+        common::erc20::events::Transfer,
+        curve::pool::events::{RemoveLiquidityOne1, RemoveLiquidityOne2},
+    },
+    types::transfer::{RemoveLiquidityTransfer, TokenTransfer},
+};
 
 pub fn extract_specific_transfer_event(
     trx: &TransactionTrace,
-    log_address: &Vec<u8>,
-    from: &Vec<u8>,
-    to: &Vec<u8>,
-) -> Result<Transfer, Error> {
+    log_address: Option<&Vec<u8>>,
+    from: Option<&Vec<u8>>,
+    to: Option<&Vec<u8>>,
+) -> Result<TokenTransfer, Error> {
+    // Count the number of Some values among the parameters
+    let provided_params_count = [log_address, from, to]
+        .iter()
+        .filter(|opt| opt.is_some())
+        .count();
+
+    // Ensure at least two params are provided. Allowing only one param is too permissive.
+    if provided_params_count < 2 {
+        return Err(anyhow!(
+            "At least two of 'log_address', 'from', or 'to' must be specified"
+        ));
+    }
+
     trx.calls
         .iter()
         .filter(|call| !call.state_reverted)
         .flat_map(|call| call.logs.iter())
         .find_map(|log| {
-            // Directly return the result of the match_and_decode if the conditions are met
+            // Check if `log_address` is provided and matches, or if it's None
+            let address_match = log_address.map_or(true, |addr| &log.address == addr);
+
             Transfer::match_and_decode(log).and_then(|transfer| {
-                if log.address == *log_address
-                    && transfer.sender == *from
-                    && transfer.receiver == *to
-                {
-                    Some(transfer)
+                // If 'from' is None, ignore receiver check; otherwise, check if it matches
+                let from_match = from.map_or(true, |from_addr| transfer.sender == *from_addr);
+
+                // If 'to' is None, ignore receiver check; otherwise, check if it matches
+                let to_match = to.map_or(true, |to_addr| transfer.receiver == *to_addr);
+
+                // If all the params match a Transfer event in the transactions logs, return it.
+                if address_match && from_match && to_match {
+                    Some(TokenTransfer::new(transfer, log.address.clone()))
                 } else {
                     None
                 }
@@ -43,4 +68,40 @@ pub fn extract_transfer_event(log: &block_view::LogView) -> Result<Transfer, Err
         .find(|log| Transfer::match_log(log))
         .ok_or_else(|| anyhow!("No transfer event found in the transaction"))
         .and_then(|log| Transfer::decode(log).map_err(|e| anyhow!(e)))
+}
+
+pub fn extract_remove_liquidity_one_event(
+    trx: &TransactionTrace,
+) -> Result<RemoveLiquidityTransfer, Error> {
+    let transfer_event = trx
+        .calls
+        .iter()
+        .filter(|call| !call.state_reverted)
+        .flat_map(|call| call.logs.iter())
+        .find_map(|log| {
+            if RemoveLiquidityOne1::match_log(log) {
+                RemoveLiquidityOne1::decode(log).ok().and_then(|ev| {
+                    Some(RemoveLiquidityTransfer::new(
+                        log.address.clone(),
+                        ev.provider,
+                        ev.token_amount,
+                        ev.coin_amount,
+                    ))
+                })
+            } else if RemoveLiquidityOne2::match_log(log) {
+                RemoveLiquidityOne2::decode(log).ok().and_then(|ev| {
+                    Some(RemoveLiquidityTransfer::new(
+                        log.address.clone(),
+                        ev.provider,
+                        ev.token_amount,
+                        ev.coin_amount,
+                    ))
+                })
+            } else {
+                None
+            }
+        });
+    transfer_event.ok_or_else(|| {
+        anyhow!("No RemoveLiquidityOne1 or RemoveLiquidityOne2 event found in the transaction")
+    })
 }
