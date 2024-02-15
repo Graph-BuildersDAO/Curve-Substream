20bcd399035213e35a82bac342a5cd554f30cb7f
jmulq <james.mulqueeny@outlook.com>
2024-02-15 13:45:45 +0000

    test: timeframe change handler unit tests

--- src/timeframe_management/timeframe_change_handler.rs
+++ src/timeframe_management/timeframe_change_handler.rs
@@ -30,3 +30,155 @@ impl<'a> TimeframeChangeHandler<'a> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    use super::*;
+    use substreams::pb::substreams::store_delta::Operation;
+    use substreams::store::{DeltaInt64, Deltas};
+
+    fn create_test_deltas(
+        operation: Operation,
+        old_value: i64,
+        new_value: i64,
+    ) -> Deltas<DeltaInt64> {
+        Deltas {
+            deltas: vec![DeltaInt64 {
+                operation: operation,
+                ordinal: 0,
+                key: String::new(), // The key isn't important for this test
+                old_value,
+                new_value,
+            }],
+        }
+    }
+
+    #[test]
+    fn test_handle_timeframe_changes_new_day() {
+        let day_triggered = Rc::new(RefCell::new(false));
+        let day_triggered_clone = day_triggered.clone();
+        let daily_deltas = create_test_deltas(Operation::Update, 1, 2); // Simulating a change from day 1 to day 2
+
+        let mut handler = TimeframeChangeHandler {
+            daily_deltas: &daily_deltas,
+            hourly_deltas: &Deltas { deltas: vec![] }, // No changes in hourly deltas for this test
+            on_new_day: Box::new(move |_| *day_triggered_clone.borrow_mut() = true),
+            on_new_hour: None,
+        };
+
+        handler.handle_timeframe_changes();
+
+        assert!(
+            *day_triggered.borrow(),
+            "Expected on_new_day to be triggered"
+        );
+    }
+
+    #[test]
+    fn test_handle_timeframe_changes_new_hour() {
+        let hour_triggered = Rc::new(RefCell::new(false));
+        let hour_triggered_clone = hour_triggered.clone();
+        let hourly_deltas = create_test_deltas(Operation::Update, 10, 11); // Simulating a change from hour 10 to hour 11
+
+        let mut handler = TimeframeChangeHandler {
+            daily_deltas: &Deltas { deltas: vec![] }, // No changes in daily deltas for this test
+            hourly_deltas: &hourly_deltas,
+            on_new_day: Box::new(|_| {}),
+            on_new_hour: Some(Box::new(move |_| *hour_triggered_clone.borrow_mut() = true)),
+        };
+
+        handler.handle_timeframe_changes();
+
+        assert!(
+            *hour_triggered.borrow(),
+            "Expected on_new_hour to be triggered"
+        );
+    }
+
+    #[test]
+    fn test_no_change_no_trigger() {
+        let day_triggered = Rc::new(RefCell::new(false));
+        let hour_triggered = Rc::new(RefCell::new(false));
+        let day_triggered_clone = day_triggered.clone();
+        let hour_triggered_clone = hour_triggered.clone();
+        let daily_deltas = create_test_deltas(Operation::Update, 2, 2); // No actual change in day
+        let hourly_deltas = create_test_deltas(Operation::Update, 11, 11); // No actual change in hour
+
+        let mut handler = TimeframeChangeHandler {
+            daily_deltas: &daily_deltas,
+            hourly_deltas: &hourly_deltas,
+            on_new_day: Box::new(|_| *day_triggered_clone.borrow_mut() = true),
+            on_new_hour: Some(Box::new(|_| *hour_triggered_clone.borrow_mut() = true)),
+        };
+
+        handler.handle_timeframe_changes();
+
+        assert!(
+            !*day_triggered.borrow(),
+            "on_new_day should not be triggered without a day change"
+        );
+        assert!(
+            !*hour_triggered.borrow(),
+            "on_new_hour should not be triggered without an hour change"
+        );
+    }
+
+    #[test]
+    fn test_operation_create_with_change_no_trigger() {
+        let day_triggered = Rc::new(RefCell::new(false));
+        let hour_triggered = Rc::new(RefCell::new(false));
+        let day_triggered_clone = day_triggered.clone();
+        let hour_triggered_clone = hour_triggered.clone();
+        let daily_deltas = create_test_deltas(Operation::Create, 0, 1); // Simulating a creation of the first daily timeframe
+        let hourly_deltas = create_test_deltas(Operation::Create, 0, 1); // Simulating a creation of the first hourly timeframe
+
+        let mut handler = TimeframeChangeHandler {
+            daily_deltas: &daily_deltas,
+            hourly_deltas: &hourly_deltas,
+            on_new_day: Box::new(|_| *day_triggered_clone.borrow_mut() = true),
+            on_new_hour: Some(Box::new(|_| *hour_triggered_clone.borrow_mut() = true)),
+        };
+
+        handler.handle_timeframe_changes();
+
+        assert!(
+            !*day_triggered.borrow(),
+            "on_new_day should not be triggered when delta is create operation"
+        );
+        assert!(
+            !*hour_triggered.borrow(),
+            "on_new_hour should not be triggered when delta is create operation"
+        );
+    }
+
+    #[test]
+    fn test_handle_timeframe_changes_no_deltas() {
+        let day_triggered = Rc::new(RefCell::new(false));
+        let hour_triggered = Rc::new(RefCell::new(false));
+        let day_triggered_clone = day_triggered.clone();
+        let hour_triggered_clone = hour_triggered.clone();
+
+        let mut handler = TimeframeChangeHandler {
+            daily_deltas: &Deltas { deltas: vec![] }, // No changes in daily deltas for this test
+            hourly_deltas: &Deltas { deltas: vec![] }, // No changes in hourly deltas for this test
+            on_new_day: Box::new(|_| *day_triggered_clone.borrow_mut() = true),
+            on_new_hour: Some(Box::new(|_| *hour_triggered_clone.borrow_mut() = true)),
+        };
+
+        handler.handle_timeframe_changes();
+
+        assert!(
+            !*day_triggered.borrow(),
+            "on_new_day should not be triggered when there are no daily deltas"
+        );
+        assert!(
+            !*hour_triggered.borrow(),
+            "on_new_hour should not be triggered when there are no hourly deltas"
+        );
+    }
+
+    // Add more tests as necessary...
+}
