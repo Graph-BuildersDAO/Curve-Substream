a786af930da19b5a1dc494161ab4e2f37e81ef0c
jmulq <james.mulqueeny@outlook.com>
2024-03-15 21:46:40 +0000

    fix: tvl swap underlying bug for non-curve base pools

--- src/modules/10_store_pool_volume_usd.rs
+++ src/modules/10_store_pool_volume_usd.rs
@@ -133,71 +133,99 @@ pub fn store_pool_volume_usd(
                     );
                 }
                 Type::SwapUnderlyingEvent(swap_underlying) => {
-                    let base_pool = pools_store
-                        .must_get_last(StoreKey::pool_key(&swap_underlying.base_pool_address));
+                    let base_pool_option = pools_store
+                        .get_last(StoreKey::pool_key(&swap_underlying.base_pool_address));
 
-                    let token_in = pool
+                    let token_in_option = pool
                         .input_tokens
                         .iter()
                         .find(|t| t.address == swap_underlying.token_in_ref().token_address);
-                    let token_out = base_pool
-                        .input_tokens
-                        .iter()
-                        .find(|t| t.address == swap_underlying.token_out_ref().token_address);
 
-                    if token_in.is_none() || token_out.is_none() {
-                        substreams::log::debug!("Token_in or Token_out not found in pool for swap event. Skipping event.");
-                        continue;
+                    if let Some(token_in) = token_in_option {
+                        let token_in_price =
+                            get_token_usd_price(&token_in, &uniswap_prices, &chainlink_prices);
+
+                        let token_in_amount_usd = token_in_price
+                            * swap_underlying
+                                .token_in_amount_big()
+                                .to_decimal(token_in.decimals);
+
+                        // If we have a Curve base pool tracked, we can incorporate
+                        // the known token price into our TVL calculations.
+                        if let Some(base_pool) = base_pool_option {
+                            let token_out_option = base_pool.input_tokens.iter().find(|t| {
+                                t.address == swap_underlying.token_out_ref().token_address
+                            });
+
+                            if let Some(token_out) = token_out_option {
+                                let token_out_price = get_token_usd_price(
+                                    &token_out,
+                                    &uniswap_prices,
+                                    &chainlink_prices,
+                                );
+                                let token_out_amount_usd = token_out_price
+                                    * swap_underlying
+                                        .token_out_amount_big()
+                                        .to_decimal(token_out.decimals);
+
+                                let volume_usd = (token_in_amount_usd.clone()
+                                    + token_out_amount_usd.clone())
+                                .div(2);
+
+                                output_store.add_many(
+                                    event.log_ordinal,
+                                    &vec![
+                                        StoreKey::pool_volume_usd_key(&event.pool_address),
+                                        StoreKey::pool_volume_usd_daily_key(
+                                            &day_id,
+                                            &event.pool_address,
+                                        ),
+                                        StoreKey::pool_volume_usd_hourly_key(
+                                            &hour_id,
+                                            &event.pool_address,
+                                        ),
+                                    ],
+                                    &volume_usd,
+                                );
+                            }
+                        } else {
+                            // If there is no Curve base pool, just use the known pools token for the TVL calculations
+                            output_store.add_many(
+                                event.log_ordinal,
+                                &vec![
+                                    StoreKey::pool_volume_usd_key(&event.pool_address),
+                                    StoreKey::pool_volume_usd_daily_key(
+                                        &day_id,
+                                        &event.pool_address,
+                                    ),
+                                    StoreKey::pool_volume_usd_hourly_key(
+                                        &hour_id,
+                                        &event.pool_address,
+                                    ),
+                                ],
+                                &token_in_amount_usd,
+                            );
+                        }
+
+                        // For SwapUnderlying events, we only need to track the volume for the tokens related to this pool.
+                        // The base pool handles the volume for the underlying token swapped out.
+                        output_store.add_many(
+                            event.log_ordinal,
+                            &vec![
+                                StoreKey::pool_token_volume_usd_daily_key(
+                                    &day_id,
+                                    &event.pool_address,
+                                    &swap_underlying.token_in_ref().token_address,
+                                ),
+                                StoreKey::pool_token_volume_usd_hourly_key(
+                                    &hour_id,
+                                    &event.pool_address,
+                                    &swap_underlying.token_in_ref().token_address,
+                                ),
+                            ],
+                            token_in_amount_usd,
+                        );
                     }
-
-                    let token_in = token_in.unwrap();
-                    let token_out = token_out.unwrap();
-
-                    let token_in_price =
-                        get_token_usd_price(&token_in, &uniswap_prices, &chainlink_prices);
-                    let token_out_price =
-                        get_token_usd_price(&token_out, &uniswap_prices, &chainlink_prices);
-
-                    let token_in_amount_usd = token_in_price
-                        * swap_underlying
-                            .token_in_amount_big()
-                            .to_decimal(token_in.decimals);
-                    let token_out_amount_usd = token_out_price
-                        * swap_underlying
-                            .token_out_amount_big()
-                            .to_decimal(token_out.decimals);
-
-                    let volume_usd =
-                        (token_in_amount_usd.clone() + token_out_amount_usd.clone()).div(2);
-
-                    output_store.add_many(
-                        event.log_ordinal,
-                        &vec![
-                            StoreKey::pool_volume_usd_key(&event.pool_address),
-                            StoreKey::pool_volume_usd_daily_key(&day_id, &event.pool_address),
-                            StoreKey::pool_volume_usd_hourly_key(&hour_id, &event.pool_address),
-                        ],
-                        &volume_usd,
-                    );
-
-                    // For SwapUnderlying events, we only need to track the volume for the tokens related to this pool.
-                    // The base pool handles the volume for the underlying token swapped out.
-                    output_store.add_many(
-                        event.log_ordinal,
-                        &vec![
-                            StoreKey::pool_token_volume_usd_daily_key(
-                                &day_id,
-                                &event.pool_address,
-                                &swap_underlying.token_in_ref().token_address,
-                            ),
-                            StoreKey::pool_token_volume_usd_hourly_key(
-                                &hour_id,
-                                &event.pool_address,
-                                &swap_underlying.token_in_ref().token_address,
-                            ),
-                        ],
-                        token_in_amount_usd,
-                    );
                 }
                 _ => {}
             }
